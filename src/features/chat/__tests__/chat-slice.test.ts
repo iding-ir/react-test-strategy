c328f4b157a38964b59eb0d517c62d09c472ee35
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2025-01-18 03:12:46 +0100

    Write tests for Redux

---
+++ src/features/chat/__tests__/chat-slice.test.ts
@@ -0,0 +1,58 @@
+import { describe, it, expect } from "vitest";
+import {
+  addMessage,
+  chatSlice,
+  clearMessages,
+  selectMessages,
+} from "../chat-slice";
+
+describe("chatSlice", () => {
+  it("should have initial state", () => {
+    // Arrange
+    const state = chatSlice.reducer(undefined, { type: "" });
+
+    // Act
+    const expected = { messages: [] };
+
+    // Assert
+    expect(state).toEqual(expected);
+  });
+
+  it("should add a message", () => {
+    // Arrange
+    const message = "Hello, world!";
+    const initialState = { messages: [] };
+
+    // Act
+    const state = chatSlice.reducer(initialState, addMessage(message));
+    const expected = { messages: [message] };
+
+    // Assert
+    expect(state).toEqual(expected);
+  });
+
+  it("should clear messages", () => {
+    // Arrange
+    const message = "Hello, world!";
+    const initialState = { messages: [message] };
+
+    // Act
+    const state = chatSlice.reducer(initialState, clearMessages());
+    const expected = { messages: [] };
+
+    // Assert
+    expect(state).toEqual(expected);
+  });
+
+  it("should select messages", () => {
+    // Arrange
+    const messages = ["Hello, world!"];
+    const state = { chat: { messages } };
+
+    // Act
+    const received = selectMessages(state);
+
+    // Assert
+    expect(received).toEqual(messages);
+  });
+});
